d71e75fc2e78f88911a64f072fc703a5ff3d1827
wramadhan <wahyuramadhan0549@gmail.com>
2022-07-19 16:26:07 +0700

    make small note for me

---
+++ mymovies-app-react/notes/fetch.js
@@ -0,0 +1,34 @@
+// import axios from "axios";
+
+/*
+URL => endpoint atau alamat dari API
+DATA/BODY => Data yang akan dikirimkan ke API
+CONFIG -> konfigurasi untuk mengirimkan ke API
+
+//UNIVERSAL
+var config = {
+  method: 'get'/'post'/'put'/'delete',
+  url: 'URL',
+  headers: {
+    'Content-Type': 'application/json', //ngasih info data yang dikirimkan adalah JSON
+    'Authorization': 'Bearer ' + token, //JWT
+  }
+};
+
+axios(config)
+
+//cara  get dengan axios
+axios.get(URL, CONFIG)
+
+//POST
+biasa untuk mengirim data ke server (login, register, create post, create, comment, etc)
+axios.post(URL, DATA, CONFIG)
+
+//PUT
+biasa digunakan untuk mengubah data record yang sudah ada di server(update post, update comment, update profile, etc)
+axios.put(URL, DATA, CONFIG)
+
+//DELETE
+biasa digunakan untuk menghapus data record yang sudah ada di server
+axios.delete(URL, CONFIG)
+*/
